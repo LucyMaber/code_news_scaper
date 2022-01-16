28a88cdcb468e7c938aa2ff3c99b6cfdbeffe9a6
William Maber <maberwilliam@gmail.com>
2022-01-16 11:52:05 +0000

    update code

--- ML/genotype/attention/adaptive_masking.py
+++
@@ -1,6 +0,0 @@
-import torch
-import torch.nn as nn
-
-class LSHSelfAttention(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
--- uitls/HackerNews.py
+++
@@ -1,5 +0,0 @@
-def getPost(id):
-    url = "https://hacker-news.firebaseio.com/v0/item/{id}".format(id =id)
-
-def getStories():
-    url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
